2b6517fa447ace9556fa78e9245be1fdd8e824ba
Niall Conaghan <niallconaghan@Nialls-MacBook-Pro-2.local>
2023-02-16 21:18:09 +0000

    feat: initial commit

---
+++ projects/ngx-calendar/src/lib/ngx-calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-calendar',
+  template: `
+    <p>
+      ngx-calendar works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxCalendarComponent {
+
+}
---
+++ projects/ngx-calendar/src/lib/ngx-calendar.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgxCalendarComponent } from './ngx-calendar.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxCalendarComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxCalendarComponent
+  ]
+})
+export class NgxCalendarModule { }
